a9969dae0458674ee130915d25e0bb81bcdaf7af
Surendiran S <37256810+surendirans7777@users.noreply.github.com>
2020-04-02 17:29:18 +0530

    Update and rename scan_some_port.py to nmapscanner.py

--- nmapscanner.py
+++ nmapscanner.py
@@ -2,11 +2,11 @@ import nmap
 import sys 
 
 target = str(sys.argv[1])
-ports = [21,80,443,8080]
+ports = [21,22,80,139,443,8080]
 
 scan_v = nmap.PortScanner()
 
-print("\nScanning",target,"for ports 21,80,443 and 8080\n")
+print("\nScanning",target,"for ports 21,22,80,139,443 and 8080...\n")
 
 for port in ports:
     portscan = scan_v.scan(target,str(port))
