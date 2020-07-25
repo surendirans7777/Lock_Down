4521f3cdb704fd805742ae813e7276e8178a96e5
Surendiran S <37256810+surendirans7777@users.noreply.github.com>
2020-07-25 09:55:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+const url = process.argv[2];
+
+if(!url){
+    throw "Please provide the website url"
+}
+
+
+async function run(){
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    await page.screenshot({path:'screenshot.png'});
+
+    browser.close();
+}
+
+run();
